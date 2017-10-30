ccd5e0d2c70363d395c040e1a80eb5712a7a3a69
Munique Araujo <muniqueeva@id.uff.br>
2017-10-30 18:05:11 -0200

    Modificando o primeiro programa para gerar um série de números aleatórios

---
+++ aleatorio.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define np 1024.0
+
+double funcao(double x)
+{	
+	double s;
+	s=sin(3*((rand()%200/100.0 - 1)));
+   	return (s);
+}
+
+void main(int argc, char **argv)
+{	double xmin, xmax, x, y, dx;
+	FILE *arquivo;
+
+	arquivo=fopen(argv[1], "w");
+	
+	xmin=-M_PI;	
+	xmax=M_PI;
+	
+	dx= (xmax - xmin)/np;
+	
+	for(x=xmin; x<xmax; x=x+dx)
+	{
+		y=funcao(x);
+		fprintf(arquivo," %lf %lf\n", x, y);
+	}	
+}
